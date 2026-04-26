e823adf5bae3219702885e89d1588278883089ae
vallalaaraathiinam <vallalaaraathiinam@gmail.com>
2026-04-26 13:40:39 +0530

    Initial Commit
    Initial commit - Draft version of website

---
+++ src/data/books.js
@@ -0,0 +1,38 @@
+export const books = [
+    {
+      id: 1,
+      titleEn: "Olineri Vedham - Part 1",
+      titleTa: "ஒளிநெறி வேதம் - பாகம் 1",
+      price: "₹450",
+      image: "/images/olineri_vedham_part1.jpg",
+      descEn: "Core Philosophies - Compilation of Secrets about Universal Truth",
+      descTa: "மூல தத்துவங்கள் - பிரபஞ்ச பேருண்மை இரகசியங்களின் தொகுப்பு"
+    },
+    {
+      id: 2,
+      titleEn: "Olineri Vedham - Part 2",
+      titleTa: "ஒளிநெறி வேதம் - பாகம் 2",
+      price: "₹150",
+      image: "/images/olineri_vedham_part2.jpg",
+      descEn: "A deep dive into the initial steps of the Path of Light - Part 2",
+      descTa: "Rules of Human Origins Compilation of Universal Truth"
+    },
+    {
+      id: 3,
+      titleEn: "The Secret of Olineri",
+      titleTa: "ஒளிநெறி ரகசியம்",
+      price: "₹150",
+      image: "/images/book1.jpg",
+      descEn: "A deep dive into the initial steps of the Path of Light.",
+      descTa: "ஒளிநெறியின் ஆரம்ப கட்ட ரகசியங்களை விளக்கும் புத்தகம்."
+    },
+    {
+      id: 4,
+      titleEn: "The Secret of Olineri",
+      titleTa: "ஒளிநெறி ரகசியம்",
+      price: "₹150",
+      image: "/images/book1.jpg",
+      descEn: "A deep dive into the initial steps of the Path of Light.",
+      descTa: "ஒளிநெறியின் ஆரம்ப கட்ட ரகசியங்களை விளக்கும் புத்தகம்."
+    },
+  ];
