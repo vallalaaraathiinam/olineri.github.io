f5102ca0de94fbff233467027f66f3cbb72108f4
vallalaaraathiinam <vallalaaraathiinam@gmail.com>
2026-04-26 13:50:15 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://olineri.github.io',
+  site: 'https://vallalaaraathiinam.github.io',
   base: '/olineri-website', // This must match your repo name exactly
   integrations: [tailwind()]
 });
