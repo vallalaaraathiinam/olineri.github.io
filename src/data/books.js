7f1c61e23d9be62bcb600a2cb85c60bca065b9dc
vallalaaraathiinam <vallalaaraathiinam@gmail.com>
2026-04-26 15:11:27 +0530

    Update books.js
    Details for 8 books

--- src/data/books.js
+++ src/data/books.js
@@ -12,27 +12,63 @@ export const books = [
       id: 2,
       titleEn: "Olineri Vedham - Part 2",
       titleTa: "ஒளிநெறி வேதம் - பாகம் 2",
-      price: "₹150",
+      price: "₹120",
       image: "/images/olineri_vedham_part2.jpg",
-      descEn: "A deep dive into the initial steps of the Path of Light - Part 2",
-      descTa: "Rules of Human Origins Compilation of Universal Truth"
+      descEn: "Rules of Human Origins - Compilation of Universal Truth",
+      descTa: "மனித கருமைய விதிகள் பிரபஞ்ச இரகசியங்களின் தொகுப்பு"
     },
     {
       id: 3,
-      titleEn: "The Secret of Olineri",
-      titleTa: "ஒளிநெறி ரகசியம்",
-      price: "₹150",
+      titleEn: "Deepathil Deiva Paavanai",
+      titleTa: "தீபத்தில் தெய்வ பாவனை",
+      price: "₹120",
       image: "/images/book1.jpg",
-      descEn: "A deep dive into the initial steps of the Path of Light.",
-      descTa: "ஒளிநெறியின் ஆரம்ப கட்ட ரகசியங்களை விளக்கும் புத்தகம்."
+      descEn: "Meditating on Lamp flame",
+      descTa: "கனல் நெறி தீப தவம்"
     },
     {
       id: 4,
-      titleEn: "The Secret of Olineri",
-      titleTa: "ஒளிநெறி ரகசியம்",
-      price: "₹150",
+      titleEn: "Arutperunjothi",
+      titleTa: "அருட்பெருஞ்சோதி",
+      price: "₹60",
       image: "/images/book1.jpg",
-      descEn: "A deep dive into the initial steps of the Path of Light.",
-      descTa: "ஒளிநெறியின் ஆரம்ப கட்ட ரகசியங்களை விளக்கும் புத்தகம்."
+      descEn: "Explanation of Arutperunojothi - The universal mantra",
+      descTa: "மகா மந்திர மறை பொருள் விளக்கம்"
+    },
+    {
+      id: 5,
+      titleEn: "Valluvar Vazhangiya Thavaneri",
+      titleTa: "வள்ளுவர் வழங்கிய தவநெறி",
+      price: "80",
+      image: "/images/olineri_vedham_part1.jpg",
+      descEn: "Secrets behind the epithets of Thirukural",
+      descTa: "குறள் கூறும் இரகசியங்கள்"
+    },
+    {
+      id: 6,
+      titleEn: "Guruvarul Magimai",
+      titleTa: "குருவருள் மகிமை",
+      price: "30",
+      image: "/images/olineri_vedham_part2.jpg",
+      descEn: "Universal Truths about Teacher's Blessings",
+      descTa: "குருவருள் பற்றிய பேருண்மைகள்"
+    },
+    {
+      id: 7,
+      titleEn: "Kadavul Aalayangal Aindhu",
+      titleTa: "கடவுள் ஆலயங்கள் ஐந்து",
+      price: "₹180",
+      image: "/images/book1.jpg",
+      descEn: "Secrets of the Spiritual Lifestyle",
+      descTa: "இறை ஞான வாழ்வியல் இரகசியங்கள்"
+    },
+    {
+      id: 8,
+      titleEn: "Tiruvadi Gnana Deetchaa Ragasiyam",
+      titleTa: "திருவடி ஞான தீட்சா இரகசியங்கள்",
+      price: "₹180",
+      image: "/images/book1.jpg",
+      descEn: "Secrets about Spiritual Initiation",
+      descTa: "திருவடி ஞான தீட்சா இரகசியங்கள், கனல் நெறி திருவடி தவம்"
     },
   ];
