8d69d6df99b257c60868bac85bae88ff245d181b
Harish <harish@Harishs-MacBook-Pro.local>
2026-04-30 12:11:17 +0530

    Save local changes before merge

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://olineri.org/',
+  base: '/', // This MUST be just a slash
+  integrations: [tailwind()]
+});
---
+++ src/data/books.js
@@ -0,0 +1,74 @@
+export const books = [
+    {
+      id: 1,
+      titleEn: "Olineri Vedham - Part 1",
+      titleTa: "ஒளிநெறி வேதம் - பாகம் 1",
+      price: "₹250",
+      image: "/images/olineri_vedham_part1.jpg",
+      descEn: "Core Philosophies - Compilation of Secrets about Universal Truth",
+      descTa: "மூல தத்துவங்கள் - பிரபஞ்ச பேருண்மை இரகசியங்களின் தொகுப்பு"
+    },
+    {
+      id: 2,
+      titleEn: "Olineri Vedham - Part 2",
+      titleTa: "ஒளிநெறி வேதம் - பாகம் 2",
+      price: "₹60",
+      image: "/images/olineri_vedham_part2.jpg",
+      descEn: "Rules of Human Origins - Compilation of Universal Truth",
+      descTa: "மனித கருமைய விதிகள் பிரபஞ்ச இரகசியங்களின் தொகுப்பு"
+    },
+    {
+      id: 3,
+      titleEn: "Deepathil Deiva Paavanai",
+      titleTa: "தீபத்தில் தெய்வ பாவனை",
+      price: "₹60",
+      image: "/images/deepathil_deiva_paavanai.jpg",
+      descEn: "Meditating on Lamp flame",
+      descTa: "கனல் நெறி தீப தவம்"
+    },
+    {
+      id: 4,
+      titleEn: "Arutperunjothi",
+      titleTa: "அருட்பெருஞ்சோதி",
+      price: "₹30",
+      image: "/images/maha_mandhira_vilakkam.jpg",
+      descEn: "Explanation of Arutperunojothi - The universal mantra",
+      descTa: "மகா மந்திர மறை பொருள் விளக்கம்"
+    },
+    {
+      id: 5,
+      titleEn: "Valluvar Vazhangiya Thavaneri",
+      titleTa: "வள்ளுவர் வழங்கிய தவநெறி",
+      price: "₹50",
+      image: "/images/valluvar_dhava_neri.jpg",
+      descEn: "Secrets behind the epithets of Thirukural",
+      descTa: "குறள் கூறும் இரகசியங்கள்"
+    },
+    {
+      id: 6,
+      titleEn: "Guruvarul Magimai",
+      titleTa: "குருவருள் மகிமை",
+      price: "₹30",
+      image: "/images/guruvarul_magimai.jpg",
+      descEn: "Universal Truths about Teacher's Blessings",
+      descTa: "குருவருள் பற்றிய பேருண்மைகள்"
+    },
+    {
+      id: 7,
+      titleEn: "Kadavul Aalayangal Aindhu",
+      titleTa: "கடவுள் ஆலயங்கள் ஐந்து",
+      price: "₹100",
+      image: "/images/kadavul_aalyangal_vilakkam_aindhu.jpg",
+      descEn: "Secrets of the Spiritual Lifestyle",
+      descTa: "இறை ஞான வாழ்வியல் இரகசியங்கள்"
+    },
+    {
+      id: 8,
+      titleEn: "Tiruvadi Gnana Deetchaa Ragasiyam",
+      titleTa: "திருவடி ஞான தீட்சா இரகசியங்கள்",
+      price: "₹100",
+      image: "/images/dheetcha_ragasiyangal.jpg",
+      descEn: "Secrets about Spiritual Initiation",
+      descTa: "திருவடி ஞான தீட்சா இரகசியங்கள், கனல் நெறி திருவடி தவம்"
+    },
+  ];
