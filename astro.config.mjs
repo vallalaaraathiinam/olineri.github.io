7bf0a0a2bfa45bd503ae38787a5d8ede5292f545
Harish <harish@Harishs-MacBook-Pro.local>
2026-05-02 18:11:28 +0530

    removed nested src folder within /src; accidentally copy/pasted

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,10 @@ import tailwind from '@astrojs/tailwind';
 // import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://olineri.org/',
+  site: 'https://olineri.org',
   base: '/', // This MUST be just a slash
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(), 
+    // sitemap()
+  ]
 });
