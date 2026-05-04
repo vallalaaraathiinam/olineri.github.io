341a5b2add98ea6aced9965a1d46edc5a1c75a8c
Harish <harish@Harishs-MacBook-Pro.local>
2026-05-04 17:19:30 +0530

    added safelistfor classes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: ['./src/**/*.{astro,html,js,jsx,md,mdx,ts,tsx}'],
+    safelist: [
+      'translate-y-0',
+      'translate-y-20',
+      'opacity-0',
+      'opacity-100',
+    ],
     theme: {
       extend: {},
     },
